c612f65ad8aeab7ccc068f31b0ac16e9fee86e19
ssamek <123518145+ssamek@users.noreply.github.com>
2023-10-12 22:15:35 -0500

    adding in js

---
+++ script.js
@@ -0,0 +1,18 @@
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+
+  document.querySelector('form').addEventListener('submit', function(event) {
+    var emailInput = document.querySelector('input[type="email"]');
+    var email = emailInput.value;
+   
+    if (!validateEmail(email)) {
+       alert('Please enter a valid email address.');
+       event.preventDefault();
+    }
+   });
+});
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+ } 
