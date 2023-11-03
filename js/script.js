b9a748311f042e3df86ced4826788f2cd0052b94
ssamek <123518145+ssamek@users.noreply.github.com>
2023-11-02 22:18:43 -0500

    taking out the js that does not work

--- js/script.js
+++ js/script.js
@@ -16,10 +16,3 @@ function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
  } 
- var form = document.getElementById('form');
-
-function alertFunction() {
-    if (form.checkValidity()) {
-        alert("Thanks for Signing Up! See you at the Elbertsons soon!");
-    }
-}
